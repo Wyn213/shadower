1fe1b5634dfa935d32e4dd8f293f8b927cc3b340
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-08 21:31:25 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -33,7 +33,7 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  document.getElementById("frame").src = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
 
 var bookMarkJson = [];
